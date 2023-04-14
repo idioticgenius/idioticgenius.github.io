41c4790e70ffa636b9bb5680eb361f2e3fc75d6f
idioticgenius <abhidanthapa@gmail.com>
2023-04-14 17:09:12 +0545

    separated vanta.js code from html to a separate js file changed netAnimation id to backgroundAnimation

--- js/portfolio_slider.js
+++ js/portfolio_slider.js
@@ -55,18 +55,4 @@ function showLargeImage(src) {
 }
 
 
-VANTA.NET({
-    el: "#netAnimation",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0x000000,
-    backgroundColor: 0x1d1d1d,
-    points: 10.00,
-    maxDistance: 30.00,
-    spacing: 15.00
-});
+
---
+++ js/vanta.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#backgroundAnimation",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x000000,
+    backgroundColor: 0x1d1d1d,
+    points: 10.00,
+    maxDistance: 30.00,
+    spacing: 15.00
+});
